92dc6dd582cf4e51471cb3d3efaf8a4cc9c77340
KiruiLangat <ted.kirui@students.ku.ac.ke>
2024-06-10 15:34:41 +0300

    Comparing MAE w/ diffrerent leaf nodes

--- HousePricingComp.py
+++ HousePricingComp.py
@@ -75,5 +75,20 @@ test_predictions = test_model.predict(val_X)
 test_mae = mean_absolute_error(val_y, test_predictions)
 print("Mean Absolute Error(Decision Tree): ", test_mae)
 
+#function to get_mae
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes, random_state = 0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+#comparing MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d \t\t Mean Absolute Error: %d" %(max_leaf_nodes, my_mae))
+
+
+
 
 
